38ca6e88a47ccc0e46b7fda32ed56d1a0b06fe0a
윾승도 <66948129+tmdeh@users.noreply.github.com>
2024-04-12 16:11:01 +0900

    test : service test 작성

--- src/test/java/org/example/tmdeh_ci_cd/TmdehCiCdApplicationTests.java
+++ src/test/java/org/example/tmdeh_ci_cd/TmdehCiCdApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.tmdeh_ci_cd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class TmdehCiCdApplicationTests {
 
   @Test
