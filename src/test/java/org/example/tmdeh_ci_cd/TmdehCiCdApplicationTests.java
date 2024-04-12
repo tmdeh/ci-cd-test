6c16ca03dfca2e048949e0f104faa17c71ecfdca
윾승도 <66948129+tmdeh@users.noreply.github.com>
2024-04-12 15:32:29 +0900

    init

---
+++ src/test/java/org/example/tmdeh_ci_cd/TmdehCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tmdeh_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdehCiCdApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
